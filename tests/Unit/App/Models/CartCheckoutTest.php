080b1917b077db4a62f935aa6a4d7b6926407493
Ramiro Varandas <ramirovarandas@users.noreply.github.com>
2022-09-30 03:36:52 -0300

    Fix missing refresh database trait on unit tests

--- tests/Unit/App/Models/CartCheckoutTest.php
+++ tests/Unit/App/Models/CartCheckoutTest.php
@@ -4,11 +4,14 @@ namespace Tests\Unit\App\Models;
 
 use App\Models\Cart;
 use App\Models\CartCheckout;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 
 class CartCheckoutTest extends TestCase
 {
+
+    use RefreshDatabase;
 
     public function test_cart_checkout_relations()
     {
--- tests/Unit/App/Models/CartItemTest.php
+++ tests/Unit/App/Models/CartItemTest.php
@@ -4,10 +4,13 @@ namespace Tests\Unit\App\Models;
 
 use App\Models\Cart;
 use App\Models\CartItem;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CartItemTest extends TestCase
 {
+
+    use RefreshDatabase;
 
     public function test_cart_item_relations()
     {
